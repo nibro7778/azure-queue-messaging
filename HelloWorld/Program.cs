cf6febdff9404e33f8c997346d55fdf671c35b7e
Niraj Trivedi <mailme7778@gmail.com>
2018-11-04 15:11:22 +1100

    Updated code to delete the message from queue

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -11,7 +11,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
 
-        //Step 1: Setting up storge account and create the queue
+            //Step 1: Setting up storge account and create the queue
 
             // Parse the connection string and return a reference to the storage account.
             var storageAccount = CloudStorageAccount.Parse(
@@ -21,21 +21,24 @@ namespace HelloWorld
             var queueClient = storageAccount.CreateCloudQueueClient();
 
             // Retrieve a reference to a container.
-            var queue = queueClient.GetQueueReference("myqueue");
+            var queue = queueClient.GetQueueReference("local-nt");
 
             // Create the queue if it doesn't already exist
             queue.CreateIfNotExists();
 
-         //Step 2: Send the message
+            //Step 2: Send the message
 
             // Create a message and add it to the queue.
-            var message = new CloudQueueMessage("Hello, World");
+            var message = new CloudQueueMessage("Hello, World final time");
             queue.AddMessage(message);
 
-         //Step 3: Receive the message
+            //Step 3: Receive the message
 
             // Get the next message
             var retrievedMessage = queue.GetMessage();
+
+            // Delete the message from queue
+            queue.DeleteMessage(retrievedMessage);
 
             // Display message.
             Console.WriteLine(retrievedMessage.AsString);
